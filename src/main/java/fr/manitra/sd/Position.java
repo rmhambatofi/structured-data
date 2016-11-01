731f0c81cf1ccfd609e85a916c88d3de3370f002
manitra <m-ra@manitra.fr>
2016-11-01 10:25:09 +0100

    Added Tree interface

---
+++ src/main/java/fr/manitra/sd/Position.java
@@ -0,0 +1,8 @@
+package fr.manitra.sd;
+
+/**
+ * Created by manitra on 01/11/2016.
+ */
+public interface Position<E> {
+    E getElement();
+}
--- src/main/java/fr/manitra/sd/Tree.java
+++ src/main/java/fr/manitra/sd/Tree.java
@@ -1,7 +1,20 @@
 package fr.manitra.sd;
 
+import java.util.Iterator;
+
 /**
  * Created by manitra on 01/11/2016.
  */
 public interface Tree<E> extends Iterable<E> {
+    Position<E> root();
+    Position<E> parent(Position<E> position) throws IllegalArgumentException;
+    Iterable<Position<E>> children(Position<E> position) throws IllegalArgumentException;
+    int numChildren(Position<E> position) throws IllegalArgumentException;
+    boolean isInternal(Position<E> position) throws IllegalArgumentException;
+    boolean isExternal(Position<E> position) throws IllegalArgumentException;
+    boolean isRoot(Position<E> position) throws IllegalArgumentException;
+    int size();
+    boolean isEmpty();
+    Iterator<E> iterator();
+    Iterator<Position<E>> positions();
 }
